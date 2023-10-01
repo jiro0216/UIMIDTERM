6d9f4ac3aec3c30590b47e86a06d5872143afdb3
Joshuaflores12 <joshuagabriellflores@gmail.com>
2023-10-01 08:28:54 -0400

    Update
    Bug fixed for 3 buttons

--- Assets/Script/ButtonsManager.cs
+++ Assets/Script/ButtonsManager.cs
@@ -5,6 +5,8 @@ using Unity.VisualScripting;
 
 public class ButtonsManager : MonoBehaviour
 {
+
+
     public Image imageToScale;
     public Image ImageToFade;
     public Image ImageToFly;
@@ -13,11 +15,20 @@ public class ButtonsManager : MonoBehaviour
     public Image ImageToFadeRight;
     public float speed;
     public float FadeDuration;
-    public Vector2 targetpos,Originalpos,ReturnPos;
-    public Vector3 targetpos3;
-    public Vector3 targetpos1, originalpos2, returnpos;
-    public Vector3 targetpos4, originalpos3;   
+    public Vector2 targetpos, Originalpos, ReturnPos,originalpos;
+    public Vector3 targetpos3,originalPos4;
+    public Vector3 targetpos2, originalpos2, returnpos2;
+    public Vector3 targetpos4, originalpos3;
     private bool isZoomOut = false;
+    private bool isfadeout = false;
+    private bool browseSequenceOut = false;
+    private bool isFadeOutRight = false;
+    private Color originalcolor;
+    private Vector3 originalScale;
+
+
+
+
     public void Zoom()
     {
         float zoomVal = 0;
@@ -26,50 +37,100 @@ public class ButtonsManager : MonoBehaviour
         isZoomOut = !isZoomOut;
     }
 
-    public void FadeTween() 
+    public void Awake()
+    {
+        originalcolor = ImageToFade.color;
+        originalpos = ImageToBrowse.transform.position;
+        originalScale = ImageToBrowse.transform.localScale;
+        originalcolor = ImageToBrowse.color;
+        originalPos4  = ImageToFadeRight.transform.position;
+    }
+    public void FadeTween()
     {
 
-        ImageToFade.DOFade(0, FadeDuration);
-       
-     
+        if (isfadeout)
+        {
+            ImageToFade.color = originalcolor;
+        }
+        else
+        {
+            ImageToFade.DOFade(0, FadeDuration);
+
+        }
+        isfadeout = !isfadeout;
     }
 
-    public void Flydown() 
+    public void Flydown()
     {
         ImageToFly.transform.DOLocalMove(targetpos, speed).OnComplete(() => Returnpos());
     }
 
-    public void Returnpos() 
+    public void Returnpos()
     {
         ImageToFly.transform.DOLocalMove(Originalpos, speed);
     }
 
     public void BrowseSequence() 
     {
-        DG.Tweening.Sequence sequence = DOTween.Sequence();
-        sequence.Append(ImageToBrowse.transform.DOLocalMove(targetpos1, speed));
-        sequence.AppendInterval(0);
-        sequence.Append(ImageToBrowse.transform.DOLocalMove(Originalpos, speed));
-        sequence.AppendInterval(0);
-        sequence.Append(ImageToBrowse.DOFade(0, FadeDuration));
+       
+
+        if (browseSequenceOut) 
+        {
+            //ImageToBrowse.transform.position = originalpos2;
+            //ImageToBrowse.color = originalcolor;
+            //ImageToBrowse.transform.localScale = originalScale;
+            DG.Tweening.Sequence sequence = DOTween.Sequence();
+            sequence.Append(ImageToBrowse.DOFade(originalcolor.a, FadeDuration));
+            sequence.AppendInterval(0);
+            sequence.Append(ImageToBrowse.transform.DOLocalMove(originalpos2, speed));
+            sequence.AppendInterval(0);
+            sequence.Append(ImageToBrowse.transform.DOLocalMove(originalpos2, speed));
+
+        }
+        else
+        {
+            DG.Tweening.Sequence sequence = DOTween.Sequence();
+            sequence.Append(ImageToBrowse.transform.DOLocalMove(targetpos2, speed));
+            sequence.AppendInterval(0);
+            sequence.Append(ImageToBrowse.transform.DOLocalMove(originalpos2, speed));
+            sequence.AppendInterval(0);
+            sequence.Append(ImageToBrowse.DOFade(0, FadeDuration));
+        }
+        browseSequenceOut = !browseSequenceOut;
+
     }
 
-    public void SequenceFadeRight() 
+    public void SequenceFadeRight()
     {
-        DG.Tweening.Sequence sequence = DOTween.Sequence();
-        sequence.Append(ImageToFadeRight.transform.DOLocalMove(targetpos3,speed));
-        sequence.AppendInterval(0);
-        sequence.Append(ImageToFadeRight.DOFade(0, FadeDuration));
+        if (isFadeOutRight) 
+        {
+            DG.Tweening.Sequence sequence =DOTween.Sequence();
+            sequence.Append(ImageToFadeRight.transform.DOLocalMove(targetpos3, speed));
+            sequence.AppendInterval(0);
+            sequence.Append(ImageToFadeRight.DOFade(originalcolor.a, FadeDuration));
+            sequence.Append(ImageToFadeRight.transform.DOLocalMove(originalPos4, speed));
+        }
+        else 
+        {
+
+            DG.Tweening.Sequence sequence = DOTween.Sequence();
+            sequence.Append(ImageToFadeRight.transform.DOLocalMove(targetpos3, speed));
+            sequence.AppendInterval(0);
+            sequence.Append(ImageToFadeRight.DOFade(0, FadeDuration));
+        }
+        isFadeOutRight = !isFadeOutRight;
+
+        
     }
 
-    public void Flyup() 
+    public void Flyup()
     {
-        ImageToFlyUp.transform.DOLocalMove(targetpos4,speed).OnComplete(() => Returnpos1());
+        ImageToFlyUp.transform.DOLocalMove(targetpos4, speed).OnComplete(() => Returnpos1());
     }
 
-    public void Returnpos1() 
+    public void Returnpos1()
     {
-    ImageToFlyUp.transform.DOLocalMove(originalpos3,speed);
+        ImageToFlyUp.transform.DOLocalMove(originalpos3, speed);
     }
 
 
