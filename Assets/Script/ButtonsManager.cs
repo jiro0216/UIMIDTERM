bae28eb043c327705a942b17713d7cf15e7244ca
jiro0216 <athazacreate@gmail.com>
2023-10-13 20:39:26 +0800

    Latest

--- Assets/Script/ButtonsManager.cs
+++ Assets/Script/ButtonsManager.cs
@@ -1,78 +1,159 @@
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
-using Unity.VisualScripting;
 
 public class ButtonsManager : MonoBehaviour
 {
-    public Image imageToScale;
-    public Image ImageToFade;
-    public Image ImageToFly;
-    public Image ImageToFlyUp;
-    public Image ImageToBrowse;
-    public Image ImageToFadeRight;
-    public float speed;
-    public float FadeDuration;
-    public Vector2 targetpos,Originalpos,ReturnPos;
-    public Vector3 targetpos3;
-    public Vector3 targetpos1, originalpos2, returnpos;
-    public Vector3 targetpos4, originalpos3;   
+    public Image image;
+
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+    private Vector3 originalScale;
+    private Color originalColor;
+
+    public Vector3 targetPosition, targetPosition2, targetPosition3, targetPosition4;
+
     private bool isZoomOut = false;
-    public void Zoom()
+    private bool isFadeOut = false;
+    private bool isFlyOut = false;
+    private bool browseSequenceOut = false;
+    private bool isFlippedOut = false;
+
+
+
+    private void Reset()
     {
+
+        image.transform.DOLocalMove(originalPosition, 0.1f);
+        image.DOFade(originalColor.a, 0.25f);
+        image.transform.DOScale(originalScale, 0.25f);
+
+    }
+    private void Awake()
+    {
+        // Store the original position, rotation, scale, and color for later reversal
+        originalPosition = image.transform.localPosition;
+        originalRotation = image.transform.localRotation; 
+        originalScale = image.transform.localScale;
+        originalColor = image.color;
+    }
+
+
+    public void Zoom() 
+    {
+        Reset();
+        
         float zoomVal = 0;
-        float targetScale = isZoomOut ? 1.0f : zoomVal;
-        imageToScale.transform.DOScale(targetScale, 0.25f);
+        float targetScale = isZoomOut ? 2.5f : zoomVal;
+        image.transform.DOScale(targetScale, 0.25f);
         isZoomOut = !isZoomOut;
     }
 
-    public void FadeTween() 
+    public void Fade()
     {
+        Reset();
 
-        ImageToFade.DOFade(0, FadeDuration);
-       
-     
+        if (isFadeOut)
+        {
+            //image.DOFade(originalColor.a, 0.25f);
+            image.color = originalColor;
+        }
+        else
+        {
+            image.DOFade(0.0f, 0.25f);
+            
+        }
+
+        isFadeOut = !isFadeOut;
+        
     }
 
-    public void Flydown() 
+    public void Fly()
     {
-        ImageToFly.transform.DOLocalMove(targetpos, speed).OnComplete(() => Returnpos());
-    }
+        Reset();
 
-    public void Returnpos() 
-    {
-        ImageToFly.transform.DOLocalMove(Originalpos, speed);
-    }
-
-    public void BrowseSequence() 
-    {
-        DG.Tweening.Sequence sequence = DOTween.Sequence();
-        sequence.Append(ImageToBrowse.transform.DOLocalMove(targetpos1, speed));
-        sequence.AppendInterval(0);
-        sequence.Append(ImageToBrowse.transform.DOLocalMove(Originalpos, speed));
-        sequence.AppendInterval(0);
-        sequence.Append(ImageToBrowse.DOFade(0, FadeDuration));
-    }
-
-    public void SequenceFadeRight() 
-    {
-        DG.Tweening.Sequence sequence = DOTween.Sequence();
-        sequence.Append(ImageToFadeRight.transform.DOLocalMove(targetpos3,speed));
-        sequence.AppendInterval(0);
-        sequence.Append(ImageToFadeRight.DOFade(0, FadeDuration));
-    }
-
-    public void Flyup() 
-    {
-        ImageToFlyUp.transform.DOLocalMove(targetpos4,speed).OnComplete(() => Returnpos1());
-    }
-
-    public void Returnpos1() 
-    {
-    ImageToFlyUp.transform.DOLocalMove(originalpos3,speed);
+        Vector3 endPosition = isFlyOut ? originalPosition : originalPosition + new Vector3(600f, 0, 0);
+        image.transform.DOLocalMove(endPosition, 0.1f);
+        isFlyOut = !isFlyOut;
     }
 
 
+    public void BrowseSequenceRight()
+    {
+        Reset();
+
+        if ( browseSequenceOut)
+        {
+            //ImageToBrowse.transform.position = originalpos2;
+            //ImageToBrowse.color = originalcolor;
+            //ImageToBrowse.transform.localScale = originalScale;
+            DG.Tweening.Sequence sequence = DOTween.Sequence();
+            sequence.Append(image.DOFade(originalColor.a, 0.25f));
+            sequence.AppendInterval(0);
+            sequence.Append(image.transform.DOLocalMove(originalPosition, 0.25f));
+            sequence.AppendInterval(0);
+            sequence.Append(image.transform.DOLocalMove(originalPosition, 0.25f));
+
+        }
+        else
+        {
+            DG.Tweening.Sequence sequence = DOTween.Sequence();
+            sequence.Append(image.transform.DOLocalMove(targetPosition, 0.25f));
+            sequence.AppendInterval(0);
+            sequence.Append(image.transform.DOLocalMove(targetPosition2, 0.1f));
+            sequence.AppendInterval(0);
+            sequence.Append(image.DOFade(0, 0.25f));
+        }
+        browseSequenceOut = !browseSequenceOut;
+
+    }
+
+    public void BrowseSequenceLeft()
+    {
+        Reset();
+
+        if (browseSequenceOut)
+        {
+            //ImageToBrowse.transform.position = originalpos2;
+            //ImageToBrowse.color = originalcolor;
+            //ImageToBrowse.transform.localScale = originalScale;
+            DG.Tweening.Sequence sequence = DOTween.Sequence();
+            sequence.Append(image.DOFade(originalColor.a, 0.25f));
+            sequence.AppendInterval(0);
+            sequence.Append(image.transform.DOLocalMove(originalPosition, 0.25f));
+            sequence.AppendInterval(0);
+            sequence.Append(image.transform.DOLocalMove(originalPosition, 0.25f));
+
+        }
+        else
+        {
+            DG.Tweening.Sequence sequence = DOTween.Sequence();
+            sequence.Append(image.transform.DOLocalMove(targetPosition3, 0.25f));
+            sequence.AppendInterval(0);
+            sequence.Append(image.transform.DOLocalMove(targetPosition4, 0.1f));
+            sequence.AppendInterval(0);
+            sequence.Append(image.DOFade(0, 0.25f));
+        }
+        browseSequenceOut = !browseSequenceOut;
 
 
+
+    }
+
+    public void Flip()
+    {
+        Reset();
+
+        if (isFlippedOut)
+        {
+            float targetRotation = isFlippedOut ? 2.5f : 0f; 
+            image.transform.DORotate(new Vector3(0f, targetRotation, 0f), 0.25f); 
+        }
+        else 
+        {
+            image.transform.DORotate(new Vector3(0.0f, 180.0f, 0.0f), 0.25f);
+        }
+
+        isFlippedOut = !isFlippedOut;
+    }
 }
